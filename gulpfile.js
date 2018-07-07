b8884f900570b631785d31a17a5584dd4bd3b2b4
杨鹏博 <obpt@sina.com>
2018-07-08 00:20:23 +0800

    add some gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,16 @@ gulp.task('lint',function(){
 
 var ts=require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+
 gulp.task('tsc',function(){
     return gulp.src(['./graphs/**/*.ts'])
     .pipe(ts(tsProject()))
     .js.pipe(gulp.dest('./build'));
 });
-    
+
+gulp.task('tsc-test',function(){
+    return gulp.src(['./test/**/*.ts'])
+    .pipe(ts(tsProject()))
+    .js.pipe(gulp.dest('./temp/test'));
+});    
 gulp.task('default',['lint','tsc']);
